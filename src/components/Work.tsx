32220908b0457fffcff1c31a3a847653151c79a7
Ansh Gupta <anshgupta456ansh@ece.du.ac.in>
2025-07-19 12:51:47 +0530

    fixed gsap

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -104,7 +104,7 @@ const Work = () => {
       </div>
 
       {/* ✅ Spacer to prevent overlap with TechStack */}
-      <div style={{ height: "100vh" }}></div>
+      <div style={{ height: "1vh" }}></div>
     </>
   );
 };
